8367d329f489aaf65886e7fc23244cb985e2407a
Daniel Alexander <danieltlegamer@gmail.com>
2026-09-12 22:57:48 +0200

    Refactor Content component for improved readability

--- frontend/src/app/components/checkout/content.jsx
+++ frontend/src/app/components/checkout/content.jsx
@@ -9,22 +9,36 @@ const containerVariants = {
      visible: {
           opacity: 1,
           transition: {
-               staggerChildren: 0.15, // delay entre animaciones de hijos
+               staggerChildren: 0.15,
           },
      },
 };
 
 const itemVariants = {
-     hidden: { opacity: 0, x: 50 }, // empieza desplazado a la derecha y oculto
-     visible: { opacity: 1, x: 0 },  // aparece en posición
+     hidden: { opacity: 0, x: 50 },
+     visible: { opacity: 1, x: 0 },
 };
 
-export default function Content(product_){
+export default function Content(product_) {
      const product = product_.product;
+
      return (
-          <>   
+          <>
                <motion.main
-                    className="mt-32 h-[82.3vh] grid flex-1 overflow-auto justify-center gap-4 grid-cols-[1.1fr_.9fr] max-[66.2rem]:grid-cols-1 max-[66.2rem]:auto-rows-fr max-[66.2rem]:content-center max-[35.2rem]:grid-cols-1 max-[35.2rem]:auto-rows-fr max-[35.2rem]:content-center"
+                    className="
+                         mt-32
+                         min-h-[82.3vh]
+                         grid
+                         flex-1
+                         overflow-auto
+                         justify-center
+                         gap-4
+                         grid-cols-[1.1fr_.9fr]
+
+                         max-[66.2rem]:grid-cols-1
+                         max-[66.2rem]:auto-rows-max
+                         max-[66.2rem]:content-start
+                    "
                     variants={containerVariants}
                     initial="hidden"
                     animate="visible"
@@ -37,7 +51,17 @@ export default function Content(product_){
                     </motion.section>
 
                     <motion.section
-                         className="flex flex-col items-start justify-start pl-20 bg-[#070707] max-[66.2rem]:pl-0 max-[66.2rem]:items-center max-[35.2rem]:pl-0 max-[35.2rem]:items-center"
+                         className="
+                              flex
+                              flex-col
+                              items-start
+                              justify-start
+                              pl-20
+                              bg-[#070707]
+
+                              max-[66.2rem]:pl-0
+                              max-[66.2rem]:items-center
+                         "
                          variants={itemVariants}
                     >
                          <Details product={product} />
@@ -45,5 +69,4 @@ export default function Content(product_){
                </motion.main>
           </>
      );
-
 }
